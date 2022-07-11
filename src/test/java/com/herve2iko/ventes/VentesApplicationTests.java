50b33576bf396cebd06c555add631818b8af5ba7
Herve2iko <ihervedelice@gmail.com>
2022-07-11 11:43:13 +0200

    main app

---
+++ src/test/java/com/herve2iko/ventes/VentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herve2iko.ventes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
