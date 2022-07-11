49d16da45b6993f43a5a616aefaeb58d884a83db
Herve2iko <ihervedelice@gmail.com>
2022-07-11 11:42:54 +0200

    principal classe main

---
+++ src/main/java/com/herve2iko/ventes/VentesApplication.java
@@ -0,0 +1,13 @@
+package com.herve2iko.ventes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentesApplication.class, args);
+	}
+
+}
